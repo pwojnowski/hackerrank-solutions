9e56e14c62cb5cf6625420d989870d0e0b036ccb
Przemysław Wojnowski <esperanto@cumego.com>
2015-03-03 20:42:54 +0100

    Added criterium for benchmarking.
    Ignoring .nrepl-port.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,5 @@
 (defproject hackerrank "0.1.0-SNAPSHOT"
   :description "Solutions to http://hackerrank.com problems"
   :url "http://hackerrank.com"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [criterium "0.4.3"]])
