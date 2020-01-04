ff1af3fa246e322a149f6d3c7c2bffc2faf75f8f
Przemysław Wojnowski <esperanto@cumego.com>
2020-01-04 13:49:29 +0100

    Add Graph based on adjacency list.

---
+++ java/src/main/java/poligon/algorithms/graphs/Graph.java
@@ -0,0 +1,54 @@
+package poligon.algorithms.graphs;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph {
+
+    private final Collection<Integer>[] vertices;
+
+    private Graph(Collection<Integer>[] vertices) {
+        this.vertices = vertices;
+    }
+
+    public static Graph createAdjList(int v) {
+        @SuppressWarnings("unchecked")
+        Graph g = new Graph(new List[v]);
+        for (int i = 0; i < g.vertices.length; ++i) {
+            g.vertices[i] = new LinkedList<>();
+        }
+        return g;
+    }
+
+    public int vertices() {
+        return vertices.length;
+    }
+
+    public int edges() {
+        return Arrays.stream(vertices).mapToInt(Collection::size).sum();
+    }
+
+    // Returns vertices adjacent to given
+    public Iterable<Integer> adj(int v) {
+        return vertices[v];
+    }
+
+    // Add edge(s) between v and w
+    public void addEdge(int v, int w) {
+        vertices[v].add(w);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(vertices() + System.lineSeparator() + edges() + System.lineSeparator());
+        for (int v = 0; v < vertices.length; ++v) {
+            for (Integer w : adj(v)) {
+                sb.append(v).append(" -> ").append(w).append(System.lineSeparator());
+            }
+        }
+        return sb.toString();
+    }
+
+}
---
+++ java/src/test/kotlin/poligon/algorithms/graphs/GraphTest.kt
@@ -0,0 +1,56 @@
+package poligon.algorithms.graphs
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class GraphTest {
+
+    private val emptyGraph = Graph.createAdjList(0)
+    private val only42Vertices = Graph.createAdjList(42)
+    private val graphWithAnomalies = graphWithAnomalies()
+
+    @Test
+    fun shouldReturnNumberOfVertices() {
+        assertEquals(emptyGraph.vertices(), 0)
+        assertEquals(only42Vertices.vertices(), 42)
+    }
+
+    @Test
+    fun shouldReturnEdgesAdjacentToVertex() {
+        assertEquals(listOf(0, 1, 2), graphWithAnomalies.adj(0))
+        assertEquals(listOf(2, 2), graphWithAnomalies.adj(1))
+        assertEquals(emptyList<Int>(), graphWithAnomalies.adj(2))
+    }
+
+    @Test
+    fun shouldReturnGraphAsString() {
+        // 0 vertices, 0 edges
+        assertEquals("0\n0\n", emptyGraph.toString())
+        assertEquals("42\n0\n", only42Vertices.toString())
+
+        val expected = """
+    |3
+    |5
+    |0 -> 0
+    |0 -> 1
+    |0 -> 2
+    |1 -> 2
+    |1 -> 2
+    |
+""".trimMargin()
+        assertEquals(expected, graphWithAnomalies.toString())
+    }
+
+    private fun graphWithAnomalies(): Graph {
+        return Graph.createAdjList(3)
+            .apply {
+                addEdge(0, 0) // adjacency list allows self loops
+                addEdge(0, 1)
+                addEdge(0, 2)
+                addEdge(1, 2)
+                addEdge(1, 2) // adjacency list allows parallel edges
+            }
+    }
+
+}
