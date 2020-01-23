afe0d6c9f9fdc58ef340223cb9ef38108d2d6c31
Przemysław Wojnowski <esperanto@cumego.com>
2020-01-23 14:42:28 +0100

    Add test case for search of shortest path.

--- java/src/test/kotlin/poligon/algorithms/graphs/PathFinderTest.kt
+++ java/src/test/kotlin/poligon/algorithms/graphs/PathFinderTest.kt
@@ -10,6 +10,10 @@ class PathFinderTest {
             krk waw
             waw gda
             waw szy
+            krk wro
+            wro poz
+            poz kos
+            krk kos
         """.trimIndent()
 
     private val sg = SymbolGraph.load(connections, " ")
@@ -18,6 +22,7 @@ class PathFinderTest {
     private val wawIdx = sg.index("waw")
     private val gdaIdx = sg.index("gda")
     private val szyIdx = sg.index("szy")
+    private val kosIdx = sg.index("kos")
 
     @Test
     fun shouldFindPathUsingRecursiveDFS() {
@@ -41,6 +46,9 @@ class PathFinderTest {
 
         assertEquals(listOf(krkIdx, wawIdx), finder.pathTo(wawIdx))
         assertEquals(listOf(krkIdx, wawIdx, szyIdx), finder.pathTo(szyIdx))
+
+        // BFS should find the shortest path:
+        assertEquals(listOf(krkIdx, kosIdx), finder.pathTo(kosIdx))
     }
 
 }
