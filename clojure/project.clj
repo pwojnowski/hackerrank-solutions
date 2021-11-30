7c8e9121dfb38473d24b867b294229cbdfa34895
Przemysław Wojnowski <esperanto@cumego.com>
2021-11-30 09:39:01 +0100

    Upgrade Clojure and criterium.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,5 +1,5 @@
 (defproject hackerrank "0.1.0-SNAPSHOT"
   :description "Solutions to http://hackerrank.com problems"
   :url "http://hackerrank.com"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [criterium "0.4.3"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [criterium "0.4.6"]])
